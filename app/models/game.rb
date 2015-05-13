987288ef31c8c17cbd73c30436f8e621836a237c
Koichi TANAKA <tanaka51.jp@gmail.com>
2015-05-14 00:46:29 +0900

    Add some logic

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,26 @@
 class Game < ActiveRecord::Base
   extend Enumerize
 
+  has_many :results
+
   validates :length_type, presence: true
 
+  validate :need_4_results
+  validate :need_100_000_points
+
   enumerize :length_type, in: %i(half east)
+
+  private
+
+  def need_4_results
+    return if results.length == 4
+
+    errors.add(:base, 'need 4 results')
+  end
+
+  def need_100_000_points
+    return if results.map(&:point).sum == 100_000
+
+    errors.add(:base, 'need 100,000 points')
+  end
 end
--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -1,7 +1,66 @@
 require 'test_helper'
 
 class GameTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = User.new
+  end
+
+  test 'should save with just 4 results' do
+    game = Game.new
+    game.results << Result.new(user: @user, position: :east,  point: 35000, score:  45, ranking: 1)
+    game.results << Result.new(user: @user, position: :south, point: 26000, score:   6, ranking: 2)
+    game.results << Result.new(user: @user, position: :west,  point: 24000, score: -16, ranking: 3)
+    game.results << Result.new(user: @user, position: :north, point: 15000, score: -35, ranking: 4)
+    assert game.save, '4 results given'
+  end
+
+  test 'should not save without a result' do
+    game = Game.new
+    assert_not game.save, 'no results given'
+  end
+
+  test 'should not save with 3 results' do
+    game = Game.new
+    game.results << Result.new(user: @user, position: :east,  point: 35000, score:  45, ranking: 1)
+    game.results << Result.new(user: @user, position: :south, point: 26000, score:   6, ranking: 2)
+    game.results << Result.new(user: @user, position: :west,  point: 24000, score: -16, ranking: 3)
+    assert_not game.save, '3 results given'
+  end
+
+  test 'should not save with 5 results' do
+    game = Game.new
+    game.results << Result.new(user: @user, position: :east,  point: 35000, score:  45, ranking: 1)
+    game.results << Result.new(user: @user, position: :south, point: 26000, score:   6, ranking: 2)
+    game.results << Result.new(user: @user, position: :west,  point: 24000, score: -16, ranking: 3)
+    game.results << Result.new(user: @user, position: :north, point: 15000, score: -35, ranking: 4)
+    game.results << Result.new(user: @user, position: :north, point: 15000, score: -35, ranking: 4)
+    assert_not game.save, '5 results given'
+  end
+
+  test 'should save with total 100000 points' do
+    game = Game.new
+    game.results << Result.new(user: @user, position: :east,  point: 35000, score:  45, ranking: 1)
+    game.results << Result.new(user: @user, position: :south, point: 26000, score:   6, ranking: 2)
+    game.results << Result.new(user: @user, position: :west,  point: 24000, score: -16, ranking: 3)
+    game.results << Result.new(user: @user, position: :north, point: 15000, score: -35, ranking: 4)
+    assert game.save, '100000 points'
+  end
+
+  test 'should not save with total 99900 points' do
+    game = Game.new
+    game.results << Result.new(user: @user, position: :east,  point: 35000, score:  45, ranking: 1)
+    game.results << Result.new(user: @user, position: :south, point: 26000, score:   6, ranking: 2)
+    game.results << Result.new(user: @user, position: :west,  point: 24000, score: -16, ranking: 3)
+    game.results << Result.new(user: @user, position: :north, point: 14900, score: -35, ranking: 4)
+    assert_not game.save, '99900 points'
+  end
+
+  test 'should not save with total 10100 points' do
+    game = Game.new
+    game.results << Result.new(user: @user, position: :east,  point: 35000, score:  45, ranking: 1)
+    game.results << Result.new(user: @user, position: :south, point: 26100, score:   6, ranking: 2)
+    game.results << Result.new(user: @user, position: :west,  point: 24000, score: -16, ranking: 3)
+    game.results << Result.new(user: @user, position: :north, point: 15000, score: -35, ranking: 4)
+    assert_not game.save, '99900 points'
+  end
 end
